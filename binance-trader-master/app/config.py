a00ac5b8b99c3d5d2378329fe51b19ff5d3a2ef2
duranro <37770311+duranro@users.noreply.github.com>
2018-03-25 04:55:18 -0500

    Add files via upload

---
+++ binance-trader-master/app/config.py
@@ -0,0 +1,9 @@
+# @yasinkuyu
+
+# Get an Key and Secret
+# https://www.binance.com/restapipub.html
+
+api_key = 'u6GLzOOP3ZNBCM2u8nBA2j2pzSv7eeh1BJF3tiPO9O0MKVBK5VDqKXIgkQwcRLJQ'
+api_secret = '7WA5hRC9qk5uSnY4dq0MHT3INmpi5ahSs3MFxok5OSxDBwd3wboas8AgMcfPLhxk'
+
+recv_window = 6000000
